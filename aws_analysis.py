33be1ad7b2cdc9d39cbd08294d48ab2316f0d2e7
Vivek Singh Bhadauriya <44261996+V-Bhadauriya@users.noreply.github.com>
2022-10-06 17:57:02 +0530

    Update aws_analysis.py

--- aws_analysis.py
+++ aws_analysis.py
@@ -18,7 +18,8 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
+    'labels': '{{ dag_run.conf.get("message") }}'
 }
 
 dag = DAG(
