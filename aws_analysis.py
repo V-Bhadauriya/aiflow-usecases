a4dfdb211d000866d78a139d659d4d199c8867d7
Vivek Singh Bhadauriya <44261996+V-Bhadauriya@users.noreply.github.com>
2022-10-06 16:33:05 +0530

    Create aws_analysis.py

---
+++ aws_analysis.py
@@ -0,0 +1,38 @@
+import logging
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+# log = logging.getLogger(__name__)
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'kubernetes_sample', default_args=default_args,
+    schedule_interval=timedelta(minutes=10), tags=['example', 'kubernetes', 'python', 'bash' ])
+
+
+python_task = KubernetesPodOperator(namespace='default',
+                                    image="python:3.6",
+                                    cmds=["python", "-c"],
+                                    arguments=["print('hello world')"],
+                                    labels={"foo": "bar"},
+                                    name="\'{{ dag_run.conf['task'] }}\'",
+                                    task_id="passing-task-python",
+                                    get_logs=True,
+                                    dag=dag
+                                    )
